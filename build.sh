44e0baa600d68f244eac7b2f3dcfb922d62642dc
NeoPilot <221231603+neopilotai@users.noreply.github.com>
2025-09-25 18:16:06 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-if [[ "$SHOULD_BUILD" == "yes" ]]; then
-  cd vscode
+if [[ "${SHOULD_BUILD:-}" == "yes" ]]; then
+  cd vscode || exit
   yarn
+
   mv product.json product.json.bak
-  cat product.json.bak | jq 'setpath(["extensionsGallery"]; {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery", "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index", "itemUrl": "https://marketplace.visualstudio.com/items"})' > product.json
+  jq 'setpath(["extensionsGallery"];
+      {"serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
+       "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
+       "itemUrl": "https://marketplace.visualstudio.com/items"})' \
+      product.json.bak > product.json
+
   cat product.json
   export NODE_ENV=production
 
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  if [[ "${TRAVIS_OS_NAME:-}" == "osx" ]]; then
     npx gulp vscode-darwin-min
   else
     npx gulp vscode-linux-x64-min
